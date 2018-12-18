107becfe704e2483378adaa1f493bb2d939a0e1b
Ignjatovic David <Ignjatovicdavid1@gmail.com>
2018-12-18 11:39:25 +0100

    3 tests done

--- list.cpp
+++ list.cpp
@@ -3,12 +3,11 @@
 
 struct _node {
   void* data;
-  struct node *next;
+  Node* next;
 };
 struct _list{
   Node* _head;
 };
-
 
 List* list_create(){
   List* list=(List*)malloc(sizeof (struct _list));
@@ -16,20 +15,30 @@ List* list_create(){
 }
 
 void list_delete(List *list){
-
+  free(list);
 }
 void list_add(List *list, void *data){
-
+  Node* node=(Node*)malloc(sizeof(struct _node));
+  node->data=data;
+  if(list->_head==0){list->_head=node;}
+  else{
+    node->next = list->_head;
+    list->_head = node;
+  }
 }
-void list_insert_after(Node *node, void *data){
 
+void list_insert_after(Node *node, void *data){
+  Node* newNode=(Node*)malloc(sizeof(struct _node));
+  newNode->data=data;
+  newNode->next=node->next;
+  node->next=newNode;
 }
 Node* list_get_first(List *list){
-  return 0;
+  return list->_head;
 }
 Node* list_get_next(Node *node){
-  return 0;
+  return node->next;
 }
 void* list_get_data(Node *node){
-  return 0;
+  return node->data;
 }
